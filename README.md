05eb8d7e5a15e7b945af1a366c5d1954a71b7d2b
ustasnov <ustasnov@gmail.com>
2024-01-14 14:48:19 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AHJ TASK TESTING
+
+![CI](https://github.com/ustasnov/ahj-testing/actions/workflows/web.yml/badge.svg)
---
+++ webpack.dev.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  // Set the mode to development or production
+  mode: 'development',
+  // Control how source maps are generated
+  devtool: 'inline-source-map',
+
+  // Spin up a server for quick development
+  devServer: {
+    port: 9000,
+    historyApiFallback: true,
+    open: true,
+    compress: true
+  },
+
+  plugins: [
+    // Only update what has changed on hot reload
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+});
