6de38cade14de8d03c88621d9196e3eb97770754
ustasnov <ustasnov@gmail.com>
2024-01-16 17:10:20 +0700

    Add card validation function

--- src/js/__tests__/validators.test.js
+++ src/js/__tests__/validators.test.js
@@ -1,4 +1,4 @@
-import { isValidInn } from "../validators";
+import { isValidInn, isValidCardNumber } from "../validators";
 
 test("should be false if inn length less then 10", () => {
   const result = isValidInn("");
@@ -16,4 +16,46 @@ test("should be false if inn length is 10", () => {
   const result = isValidInn("1234567890");
 
   expect(result).toBe(true);
+});
+
+test("should be true if cardNumber include only digits", () => {
+  //const result = isValidCardNumber('4111111111111111');
+  //const result = isValidCardNumber('4556737586899855');
+  const result = isValidCardNumber("4274274404160402");
+  if (!result.success) {
+    console.log(result.errorMessage);
+  }
+
+  expect(result.success).toBe(true);
+  //expect(result.success).toBe(false);
+});
+
+test("should be false if cardNumber not include only digits", () => {
+  const result = isValidCardNumber("!,6рол789ghjhg1234");
+  if (!result.success) {
+    console.log(result.errorMessage);
+  }
+
+  expect(result.success).toBe(false);
+});
+
+test("should be valid cardNumber", () => {
+  //const result = isValidCardNumber('4111111111111111');
+  //const result = isValidCardNumber('4556737586899855');
+  const result = isValidCardNumber("4274274404160402");
+  if (!result.success) {
+    console.log(result.errorMessage);
+  }
+
+  expect(result.success).toBe(true);
+  //expect(result.success).toBe(false);
+});
+
+test("should be invalid cardNumber", () => {
+  const result = isValidCardNumber("4111111111111112");
+  if (!result.success) {
+    console.log(result.errorMessage);
+  }
+
+  expect(result.success).toBe(false);
 });
--- src/js/validators.js
+++ src/js/validators.js
@@ -1,3 +1,39 @@
 export function isValidInn(value) {
   return value.length >= 10 && value.length <= 12;
 }
+
+function isValidLuhn(cardNumber) {
+  const checksum = parseInt(cardNumber[cardNumber.length - 1]);
+  let total = 0;
+
+  for (let i = cardNumber.length - 2; i >= 0; i -= 1) {
+    let digit = parseInt(cardNumber[i]);
+    if (i % 2 === 0) {
+      digit *= 2;
+    }
+    if (digit > 9) {
+      digit -= 9;
+    }
+    total += digit;
+  }
+
+  return 10 - (total % 10) === checksum;
+}
+
+export function isValidCardNumber(cardNumber) {
+  const result = { success: true, errorMessage: "", paySystem: "" };
+  // номер карты должен состоять только из цифр
+  if (!/^\d+$/.test(cardNumber)) {
+    result.success = false;
+    result.errorMessage = "The card number must consist of digits only";
+    return result;
+  }
+  // проверяем номер карты по алгоритму Luhn
+  if (!isValidLuhn(cardNumber)) {
+    result.success = false;
+    result.errorMessage = "Not valid card number";
+    return result;
+  }
+
+  return result;
+}
