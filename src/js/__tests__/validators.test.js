07adcb4d132f37dc12bcbbe356b3513673e8e47c
ustasnov <ustasnov@gmail.com>
2024-01-18 02:21:52 +0700

    Changes for validators.js and test

--- src/js/__tests__/validators.test.js
+++ src/js/__tests__/validators.test.js
@@ -18,44 +18,76 @@ test("should be false if inn length is 10", () => {
   expect(result).toBe(true);
 });
 
-test("should be true if cardNumber include only digits", () => {
-  //const result = isValidCardNumber('4111111111111111');
-  //const result = isValidCardNumber('4556737586899855');
-  const result = isValidCardNumber("4274274404160402");
-  if (!result.success) {
-    console.log(result.errorMessage);
-  }
+test("should be true if card number include only digits", () => {
+  const result = isValidCardNumber("4111111111111111");
 
   expect(result.success).toBe(true);
-  //expect(result.success).toBe(false);
 });
 
-test("should be false if cardNumber not include only digits", () => {
+test("should be false if card number not include only digits", () => {
   const result = isValidCardNumber("!,6рол789ghjhg1234");
-  if (!result.success) {
-    console.log(result.errorMessage);
-  }
 
   expect(result.success).toBe(false);
 });
 
-test("should be valid cardNumber", () => {
-  //const result = isValidCardNumber('4111111111111111');
-  //const result = isValidCardNumber('4556737586899855');
-  const result = isValidCardNumber("4274274404160402");
-  if (!result.success) {
-    console.log(result.errorMessage);
-  }
+test("should be valid card number", () => {
+  const result = isValidCardNumber("4111111111111111");
 
   expect(result.success).toBe(true);
-  //expect(result.success).toBe(false);
 });
 
-test("should be invalid cardNumber", () => {
+test("should be invalid card number", () => {
   const result = isValidCardNumber("4111111111111112");
-  if (!result.success) {
-    console.log(result.errorMessage);
+
+  expect(result.success).toBe(false);
+});
+
+test.each([
+  ["4111111111111111", "Visa"],
+  ["5555555555554444", "MasterCard"],
+  ["371449635398431", "American Express"],
+  ["6011111111111117", "Discover"],
+  ["3530111333300000", "JCB"],
+  ["30569309025904", "Diners Club"],
+  ["2201144495995333", "MIR"],
+])(
+  "%s: cards number must be valid, and from pay system %s",
+  (cardNumber, paySystem) => {
+    const destObject = { sc: true, ps: paySystem };
+    const validationResult = isValidCardNumber(cardNumber);
+    const sourceObject = {
+      sc: validationResult.success,
+      ps: validationResult.paySystem,
+    };
+
+    expect(sourceObject).toEqual(destObject);
   }
+);
+
+test.each([
+  ["411111111117", "Visa"],
+  ["5555555555554", "MasterCard"],
+  ["3714496353981", "American Express"],
+  ["6011111111119", "Discover"],
+  ["3530111333301", "JCB"],
+  ["3056930902597", "Diners Club"],
+  ["2201144495993", "MIR"],
+])(
+  "should %s: cards number has invalid card number length for pay system %s",
+  (cardNumber, paySystem) => {
+    const destObject = { sc: false, ps: paySystem };
+    const validationResult = isValidCardNumber(cardNumber);
+    const sourceObject = {
+      sc: validationResult.success,
+      ps: validationResult.paySystem,
+    };
+
+    expect(sourceObject).toEqual(destObject);
+  }
+);
+
+test("should card pay system is not defined", () => {
+  const result = isValidCardNumber("9156765567823456");
 
   expect(result.success).toBe(false);
 });
--- src/js/validators.js
+++ src/js/validators.js
@@ -2,26 +2,27 @@ export function isValidInn(value) {
   return value.length >= 10 && value.length <= 12;
 }
 
-function isValidLuhn(cardNumber) {
-  const checksum = parseInt(cardNumber[cardNumber.length - 1]);
-  let total = 0;
+function isValidLuhn(value) {
+  let nCheck = 0,
+    bEven = false;
+  //value = value.replace(/\D/g, "");
 
-  for (let i = cardNumber.length - 2; i >= 0; i -= 1) {
-    let digit = parseInt(cardNumber[i]);
-    if (i % 2 === 0) {
-      digit *= 2;
-    }
-    if (digit > 9) {
-      digit -= 9;
-    }
-    total += digit;
+  for (var n = value.length - 1; n >= 0; n--) {
+    var cDigit = value.charAt(n),
+      nDigit = parseInt(cDigit, 10);
+
+    if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
+
+    nCheck += nDigit;
+    bEven = !bEven;
   }
-
-  return 10 - (total % 10) === checksum;
+  return nCheck % 10 == 0;
 }
 
 export function isValidCardNumber(cardNumber) {
   const result = { success: true, errorMessage: "", paySystem: "" };
+  const numberLength = cardNumber.length;
+
   // номер карты должен состоять только из цифр
   if (!/^\d+$/.test(cardNumber)) {
     result.success = false;
@@ -35,5 +36,97 @@ export function isValidCardNumber(cardNumber) {
     return result;
   }
 
+  const innRangeStart2 = parseInt(cardNumber.slice(0, 2));
+  const innRangeStart3 = parseInt(cardNumber.slice(0, 3));
+  const innRangeStart4 = parseInt(cardNumber.slice(0, 4));
+  const innRangeStart6 = parseInt(cardNumber.slice(0, 6));
+
+  // определяем платежную систему и проверяем длину номера карты
+
+  // Visa
+  if (cardNumber[0] === "4") {
+    result.paySystem = "Visa";
+    if (![13, 16, 19].includes(numberLength)) {
+      result.success = false;
+      result.errorMessage = "Invalid card number length";
+    }
+    return result;
+  }
+
+  // MasterCard
+  if (
+    (innRangeStart6 >= 222100 && innRangeStart6 <= 272099) ||
+    (innRangeStart2 >= 51 && innRangeStart2 <= 55)
+  ) {
+    result.paySystem = "MasterCard";
+    if (numberLength !== 16) {
+      result.success = false;
+      result.errorMessage = "Invalid card number length";
+    }
+    return result;
+  }
+
+  // American Express
+  if ([34, 37].includes(innRangeStart2)) {
+    result.paySystem = "American Express";
+    if (numberLength !== 15) {
+      result.success = false;
+      result.errorMessage = "Invalid card number length";
+    }
+    return result;
+  }
+
+  // Discover
+  if (
+    innRangeStart4 === 6011 ||
+    innRangeStart2 === 65 ||
+    (innRangeStart3 >= 644 && innRangeStart3 <= 649) ||
+    (innRangeStart6 >= 622126 && innRangeStart6 <= 622925)
+  ) {
+    result.paySystem = "Discover";
+    if (![16, 19].includes(numberLength)) {
+      result.success = false;
+      result.errorMessage = "Invalid card number length";
+    }
+    return result;
+  }
+
+  // JCB
+  if (innRangeStart4 >= 3528 && innRangeStart4 <= 3589) {
+    result.paySystem = "JCB";
+    if (![16, 19].includes(numberLength)) {
+      result.success = false;
+      result.errorMessage = "Invalid card number length";
+    }
+    return result;
+  }
+
+  // Diners Club
+  if (
+    (innRangeStart4 >= 3000 && innRangeStart4 <= 3059) ||
+    innRangeStart4 === 3095 ||
+    innRangeStart2 === 36 ||
+    innRangeStart2 === 38
+  ) {
+    result.paySystem = "Diners Club";
+    if (![14, 16].includes(numberLength)) {
+      result.success = false;
+      result.errorMessage = "Invalid card number length";
+    }
+    return result;
+  }
+
+  // MIR
+  if (innRangeStart4 >= 2200 && innRangeStart4 <= 2204) {
+    result.paySystem = "MIR";
+    if (![16, 19].includes(numberLength)) {
+      result.success = false;
+      result.errorMessage = "Invalid card number length";
+    }
+    return result;
+  }
+
+  result.success = false;
+  result.errorMessage = "Payment system is not defined";
   return result;
 }
